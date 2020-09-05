f349d885edab8b3092bd733225aef5a0193c7459
Mr.Wang <619511821@qq.com>
2020-09-05 16:14:08 +0800

    QT不同情况港股通代码提交

--- 量化/hk_hold/models/QT_models.py
+++ 量化/hk_hold/models/QT_models.py
@@ -10,22 +10,22 @@ metadata = Base.metadata
 class HK_HOLD(Base):
     __tablename__ = 'hk_hold'
 
-    HDDATE = Column(Integer, primary_key=True, nullable=False,comment = '日期')
-    HKCODE = Column(String(16), primary_key=True, nullable=False,comment='HK代码')
-    SCODE = Column(String(16),comment="A股代码")
-    SNAME = Column(String(128),comment = 'A股名称')
-    SHAREHOLDSUM = Column(Float(asdecimal=True),comment = "持股量")
-    SHARESRATE = Column(DECIMAL(10,4),comment = "持股比例")
-    CLOSEPRICE = Column(DECIMAL(10,4),comment="收盘价")
-    ZDF = Column(Float(asdecimal=True),doc="涨跌幅")
-    SHAREHOLDPRICE = Column(Float(asdecimal=True),comment="持股市值")
-    SHAREHOLDPRICEONE = Column(Float(asdecimal=True),comment="持股市值一日变化")
-    SHAREHOLDPRICEFIVE = Column(Float(asdecimal=True),comment="持股市值五日变化")
-    SHAREHOLDPRICETEN = Column(Float(asdecimal=True),comment="持股市值十日变化")
-    MARKET = Column(String(16),comment="市场")
-    ShareHoldSumChg = Column(Float(asdecimal=True),comment="未知")
-    Zb = Column(Text,comment="未知")
-    Zzb = Column(Text,comment="未知")
+    hddate = Column(Integer, primary_key=True, nullable=False,comment = '日期')
+    hkcode = Column(String(16), primary_key=True, nullable=False,comment='HK代码')
+    scode = Column(String(16),comment="A股代码")
+    sname = Column(String(128),comment = 'A股名称')
+    shareholdsum = Column(Float(asdecimal=True),comment = "持股量")
+    sharesrate = Column(DECIMAL(10,4),comment = "持股比例")
+    closeprice = Column(DECIMAL(10,4),comment="收盘价")
+    zdf = Column(Float(asdecimal=True),doc="涨跌幅")
+    shareholdprice = Column(Float(asdecimal=True),comment="持股市值")
+    shareholdpriceone = Column(Float(asdecimal=True),comment="持股市值一日变化")
+    shareholdpricefive = Column(Float(asdecimal=True),comment="持股市值五日变化")
+    shareholdpriceten = Column(Float(asdecimal=True),comment="持股市值十日变化")
+    market = Column(String(16),comment="市场")
+    shareholdsumchg = Column(Float(asdecimal=True),comment="未知")
+    zb = Column(Text,comment="未知")
+    zzb = Column(Text,comment="未知")
 
     def columns(self):
         return [c.name for c in self.__table__.columns]
--- 量化/hk_hold/test.py
+++
@@ -1,6 +0,0 @@
-from models.QT_models import HK_HOLD
-from sqlalchemy import  Table,Column
-
-a = ["4","c"]
-b = tuple(i for i in a)
-print(b)
