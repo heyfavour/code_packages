dcf6935c60b05b6e00f04b23813bf05a2bcb5671
Mr.Wang <619511821@qq.com>
2020-09-05 16:12:01 +0800

    提交代码

--- airflow/dags/hk_hold/db/session.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-url = "mysql://root:wzx940516@49.235.242.224/QT?charset=utf8"
-engine = create_engine(url, pool_pre_ping=True)
-SessionLocal = sessionmaker(bind=engine,autocommit=True)
-session = SessionLocal()
