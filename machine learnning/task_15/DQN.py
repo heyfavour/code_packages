8f9a55c219fab85a32be4562e21e4716241c2508
heyfavourite <619511821@qq.com>
2021-06-15 17:40:34 +0800

    DQN

--- machine learnning/task_15/DQN.py
+++ machine learnning/task_15/DQN.py
@@ -7,7 +7,7 @@ import gym
 BATCH_SIZE = 64  # 每一批的训练量
 LR = 0.001  # 学习率
 EPSILON = 0.9  # 贪婪策略指数，Q-learning的一个指数，用于指示是探索还是利用。
-GAMMA = 0.7  # reward discount
+GAMMA = 0.9  # reward discount
 TARGET_REPLACE_ITER = 100  # target的更新频率
 MEMORY_CAPACITY = 1000#memery较小时，训练较快，但是后期效果没记忆体大好，建议动态记忆体，前期记忆体小，后期记忆体大
 
@@ -63,11 +63,14 @@ class DQN_Agent(object):
 
     # 选择动作
     def choose_action(self, state):
-        state = torch.unsqueeze(torch.FloatTensor(state), 0)  # [4]
+        state = torch.unsqueeze(torch.FloatTensor(state), 0)  # [4]=>[1 4]
         # input only one sample
         # np.random.uniform() 生成 0-1之间的小数
         if np.random.uniform() < EPSILON:  # 贪婪策略
-            actions_value = self.eval_net.forward(state)
+            actions_value = self.eval_net.forward(state)#[1 2]
+            #actions_value = self.eval_net(state)#[1 2]
+            #torch.max(actions_value) 返回最大的value
+            #torch.max(actions_value,1) 返回最大的(value index)
             action = torch.max(actions_value, 1)[1].data.numpy()  # return the argmax index
             action = action[0] if ENV_A_SHAPE == 0 else action.reshape(ENV_A_SHAPE)  # 如果action是多维度的则变形
         else:  # random
@@ -77,7 +80,7 @@ class DQN_Agent(object):
 
     # 存储记忆
     def store_transition(self, state, action, reward, next_state):
-        transition = np.hstack((state, [action, reward], next_state))  # 将每个参数打包起来 4+1+1+4 10
+        transition = np.hstack((state, [action, reward], next_state))  # 将每个参数打包起来 4+1+1+4  [10]
         # replace the old memory with new memory
         index = self.memory_counter % MEMORY_CAPACITY
         self.memory[index, :] = transition
@@ -91,16 +94,16 @@ class DQN_Agent(object):
         self.learn_step_counter = self.learn_step_counter + 1
 
         # 学习过程
-        sample_index = np.random.choice(MEMORY_CAPACITY, BATCH_SIZE)  # 2000中取多少个数
+        sample_index = np.random.choice(MEMORY_CAPACITY, BATCH_SIZE)  # 2000中取一个batch index [batch_size]
         sample_memory = self.memory[sample_index, :]
-        sample_state = torch.FloatTensor(sample_memory[:, :N_STATES])
+        sample_state = torch.FloatTensor(sample_memory[:, :N_STATES])#[64 4]
         sample_action = torch.LongTensor(sample_memory[:, N_STATES:N_STATES + 1].astype(int))
         sample_reward = torch.FloatTensor(sample_memory[:, N_STATES + 1:N_STATES + 2])
         sample_next_state = torch.FloatTensor(sample_memory[:, -N_STATES:])
 
         # q_eval w.r.t the action in experience
-        # shape (batch, 1) [32,2]=>[32 1]=[batch_size action_prob]
-        q_eval = self.eval_net(sample_state).gather(1,sample_action)
+        # input [batch_size dim_state] => output (batch, 2) [32,2]=>[32 1]=[batch_size action]
+        q_eval = self.eval_net(sample_state).gather(1,sample_action)#去对应的acion的实际output
         # detach的作用就是不反向传播去更新，因为target的更新在前面定义好了的 [batch_size action]
         q_next = self.target_net(sample_next_state).detach()
         #Q = r + GAMMA*MAX(Q)
@@ -119,15 +122,15 @@ if __name__ == '__main__':
         state = env.reset()  # 搜集当前环境状态。
         epoch_rewards = 0
         while True:
-            env.render()
+            #env.render()
             action = dqn.choose_action(state)
             # take action
             next_state, reward, done, info = env.step(action)
             # modify the reward 如果不重定义分数，相当难收敛
-            # x, x_dot, theta, theta_dot = next_state  # (位置x，x加速度, 偏移角度theta, 角加速度)
-            # r1 = (env.x_threshold - abs(x)) / env.x_threshold - 0.8
-            # r2 = (env.theta_threshold_radians - abs(theta)) / env.theta_threshold_radians - 0.5
-            # reward = r1 + r2
+            x, x_dot, theta, theta_dot = next_state  # (位置x，x加速度, 偏移角度theta, 角加速度)
+            r1 = (env.x_threshold - abs(x)) / env.x_threshold - 0.8
+            r2 = (env.theta_threshold_radians - abs(theta)) / env.theta_threshold_radians - 0.5
+            reward = r1 + r2
 
             epoch_rewards = epoch_rewards + reward
 
@@ -135,7 +138,7 @@ if __name__ == '__main__':
 
             if dqn.memory_counter > MEMORY_CAPACITY:  # 记忆超过2000次后
                 dqn.learn()
-                if done: print(f'Epoch: {epoch:0=3d} | epoch_rewards:  {round(epoch_rewards, 2)} |learn_step_counter {dqn.learn_step_counter}|memory_counter: {dqn.memory_counter}')
+                if done: print(f'Epoch: {epoch:0=3d} | epoch_rewards:  {round(epoch_rewards, 2)}')
             if done:
                 break
             state = next_state
