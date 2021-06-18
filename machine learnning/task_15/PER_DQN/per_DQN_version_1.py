78be9d6490f97342dda8d590b2e560275d0e0c06
Mr.Wang <619511821@qq.com>
2021-06-19 00:48:14 +0800

    PER version 1

--- machine learnning/task_15/PER_DQN/per_DQN_version_1.py
+++ machine learnning/task_15/PER_DQN/per_DQN_version_1.py
@@ -1,9 +1,8 @@
 import gym
 import torch
+import random
 import numpy as np
 import torch.nn as nn
-
-from PER_DQN.memery import Memory
 
 # 定义参数
 BATCH_SIZE = 64  # 每一批的训练量
@@ -18,6 +17,103 @@ N_ACTIONS = env.action_space.n  # 2 2个动作
 N_STATES = env.observation_space.shape[0]  # 4 state的维度
 ENV_A_SHAPE = 0 if isinstance(env.action_space.sample(), int) else env.action_space.sample().shape
 
+
+class SumTree:  # p越大，越容易sample到
+    # idx tree的索引
+    # write data的索引
+    # idx = self.wirte + self.capacity - 1
+
+    wirte = 0  # 此时写入的位置
+
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.tree = np.zeros(2 * capacity - 1)
+        self.data = np.zeros(capacity, dtype=object)
+        self.length = 0
+
+    def _propagate(self, idx, change):  # 递归将变化更新到parent_node
+        parent = (idx - 1) // 2
+        self.tree[parent] = self.tree[parent] + change  # 将变化写入父节点
+        if parent != 0: self._propagate(parent, change)  # p值更新后，其上面的父节点也都要更新
+
+    def update(self, idx, p):
+        change = p - self.tree[idx]  # 当前p-原来存的p
+        self.tree[idx] = p  # 节点写入新的p
+        self._propagate(idx, change)
+
+    def add(self, p, data):  # p sample的概率，也是error的大小 data state 需要储存的数据
+        self.data[self.wirte] = data
+        idx = self.wirte + self.capacity - 1  # idx=>tree.idx
+        self.update(idx, p)
+
+        self.wirte = self.wirte + 1
+        if self.wirte >= self.capacity: self.wirte = 0  # 叶结点存满了就从头开始覆写
+        if self.length < self.capacity: self.length = self.length + 1
+
+    # find sample on leaf node
+    def _retrieve(self, idx, sample):  ## 检索s值
+        left = 2 * idx + 1
+        right = left + 1
+        if left >= len(self.tree): return idx  ## 说明已经超过叶结点了 返回传入的idx len(tree)=2 * capacity - 1
+        if sample <= self.tree[left]:  # 比左边小 传到左
+            return self._retrieve(left, sample)  # 递归调用
+        else:  # 比左边大，sample-p
+            return self._retrieve(right, sample - self.tree[left])
+
+    def get(self, sample):
+        idx = self._retrieve(0, sample)
+        dataIdx = idx - self.capacity + 1
+        return (idx, self.tree[idx], self.data[dataIdx])  # idx p (s a r s)
+
+    def total(self):
+        return self.tree[0]
+
+class Memory:  # stored as ( s, a, r, s_ ) in SumTree
+    alpha = 0.6  # [0~1] Importance Sampling.alpha=0 退化为均匀采样
+    epsilon = 0.01  # small amount to avoid zero priority
+
+    beta = 0.4
+    beta_increment_per_sampling = 0.001
+    clip_error = 1.  # clipped abs error
+
+    def __init__(self, capacity):
+        self.tree = SumTree(capacity)
+
+    def _get_priority(self, error):  # p的范围在[epsilon,clip_error]
+        error = np.abs(error) + self.epsilon  # convert to abs and avoid 0
+        clip_error = np.minimum(error, self.clip_error)
+        return clip_error ** self.alpha  # error越大p越大 但是p有上线
+
+    def add(self, error, sample):
+        p = self._get_priority(error)
+        self.tree.add(p, sample)
+
+    def update(self, idx, error):
+        p = self._get_priority(error)
+        self.tree.update(idx, p)
+
+    def sample(self, num):
+        batch = []
+        idxs = []
+        priorities = []
+
+        segment = self.tree.total() / num
+        self.beta = np.min([1., self.beta + self.beta_increment_per_sampling])  # 0.4 + 0.01->1
+
+        for i in range(num):
+            a = segment * i
+            b = segment * (i + 1)
+            sample = random.uniform(a, b)
+            (idx, p, data) = self.tree.get(sample)
+            priorities.append(p)
+            batch.append(data)
+            idxs.append(idx)
+
+        sampling_probabilities = priorities / self.tree.total()
+        # ISWeight = (N*Pj)^(-beta) / maxi_wi
+        is_weight = np.power(self.tree.length * sampling_probabilities, -self.beta)  # (2000*p/p_sum)^-a MF （p/min_p)^-a
+        is_weight = is_weight / is_weight.max()  #
+        return batch, idxs, is_weight  #
 
 def weights_init(m):
     classname = m.__class__.__name__
