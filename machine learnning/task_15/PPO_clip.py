fa9ed16df98e38fac44047cf8e6525c8d0926182
heyfavourite <619511821@qq.com>
2021-07-05 17:29:59 +0800

    PPO TMP COMIT

--- machine learnning/task_15/PPO_clip.py
+++ machine learnning/task_15/PPO_clip.py
@@ -1,6 +1,7 @@
 """
 相当稳定的一种算法
 收敛比DQN稳定 400step分数突变过万（哭泣，DQN可以快速收敛到2000-3000但是相当不稳定,以后再也不用DQN了）
+吐槽一句理解了好久，没有DQN好理解
 """
 # !/usr/bin/env python
 # coding=utf-8
@@ -17,6 +18,7 @@ env = env.unwrapped  # 还原env的原始设置，env外包了一层防作弊层
 N_ACTIONS = env.action_space.n  # 2 2个动作
 N_STATES = env.observation_space.shape[0]  # 4 state的维度
 GAMMA = 0.99
+
 
 class Actor(nn.Module):
     def __init__(self, ):
@@ -53,38 +55,35 @@ class Critic(nn.Module):
 
 
 class PPOMemory:
-    def __init__(self, batch_size):
-        self.states = []
-        self.probs = []
-        self.vals = []
-        self.actions = []
-        self.rewards = []
-        self.dones = []
-        self.batch_size = batch_size
+    def __init__(self, memery_size, batch_step):
+        self.memory = np.zeros(memery_size, dtype=object)
+        self.memory_size = memery_size
+        self.batch_step = batch_step
+
+        self.memory_counter = 0
+
+    def add(self, transition):  # (state, action, reward, done, prob, critic)
+        index = self.memory_counter % self.memory_size
+        self.memory[index] = transition
+        self.memory_counter = self.memory_counter + 1
 
     def sample(self):
-        batch_step = np.arange(0, len(self.states), self.batch_size)  # [0 S 32步长]
-        indices = np.arange(len(self.states), dtype=np.int64)
+        # 每100步算一个epoch 每一个epoch更新memory_size/batch_step 次
+        min_batch_range = np.arange(0, self.memory_size, self.batch_step)
+        indices = np.arange(self.memory_size, dtype=np.int64)
         np.random.shuffle(indices)
-        batches = [indices[i:i + self.batch_size] for i in batch_step]  # batch_step个 batch_size大小得随机数组
-        return np.array(self.states), np.array(self.actions), np.array(self.probs), np.array(self.vals), np.array(
-            self.rewards), np.array(self.dones), batches
+        batch_index = [indices[i:i + self.batch_step] for i in min_batch_range]
+        return batch_index
 
-    def add(self, state, action, probs, vals, reward, done):
-        self.states.append(state)
-        self.actions.append(action)
-        self.probs.append(probs)
-        self.vals.append(vals)
-        self.rewards.append(reward)
-        self.dones.append(done)
-
-    def clear(self):
-        self.states = []
-        self.probs = []
-        self.actions = []
-        self.rewards = []
-        self.dones = []
-        self.vals = []
+    def data(self):  # (state, action, reward, done, prob, critic)
+        memory = np.array(list(self.memory), dtype=object).transpose()
+        state = torch.FloatTensor(np.vstack(memory[0]))
+        action = torch.LongTensor(list(memory[1])).view(-1, 1)
+        reward = torch.FloatTensor(list(memory[2])).view(-1, 1)
+        done = torch.FloatTensor(memory[4].astype(int)).view(-1, 1)
+        prob = torch.FloatTensor(list(memory[5])).view(-1, 1)
+        critic = torch.FloatTensor(list(memory[6])).view(-1, 1)
+        return state, action, reward, done, prob, critic
 
 
 class PPO():
@@ -98,41 +97,53 @@ class PPO():
         self.critic = Critic()
         self.critic_optimizer = optim.AdamW(self.critic.parameters(), lr=0.0005)
 
-        self.cov_var = torch.full(size=(self.act_dim,), fill_value=0.5)
-        self.cov_mat = torch.diag(self.cov_var)
-
-        self.batch_size = 64
-        self.memory = PPOMemory(self.batch_size)
+        self.memory_size = 100
+        self.batch_step = 10#每个batch多少步
+        self.memory = PPOMemory(self.memory_size, self.batch_step)
 
     def choose_action(self, state):
-        state = torch.tensor([state], dtype=torch.float)
-        # action
-        dist = self.actor(state)
-        action = dist.sample()
-        probs = torch.squeeze(dist.log_prob(action)).item()
-        action = torch.squeeze(action).item()
-        # value
-        value = self.critic(state)
-        value = torch.squeeze(value).item()
-        return action, probs, value
+        state = torch.unsqueeze(torch.FloatTensor(state), 0)  # [4]=>[1 4]
+        with torch.no_grad():
+            state = torch.FloatTensor(state)
+            # action
+            action_dist = self.actor(state)
+            action = action_dist.sample()
+            action_prob = action_dist.log_prob(action)
+            # value
+            critic = self.critic(state)
+        return action.item(), action_prob.item(), critic.item()
 
-    def compute_advantage(self, reward, value, done):
-        advantage = np.zeros(self.batch_size, dtype=np.float32)  # [batch_size]
-        for i in range(self.batch_size - 1):
-            discount, Q = 1, 0
-            for k in range(i, self.batch_size - 1):
-                # a = a + d*(r + (1-D)*G*V[K+1] - V[K])
-                Q = Q + discount * (reward[k] + GAMMA * value[k + 1] * (1 - done[k]) - value[k])
+    def compute_advantage(self, reward, critic, done):  # GAE方法获取returns
+        advantage = np.zeros(self.memory.memory_size, dtype=np.float32)  # [batch_size]
+        for i in range(self.memory.memory_size - 1):#[0 1 2 3 4 5 6 7 8 9]
+            discount, returns = 1, 0
+            for step in range(i, self.memory.memory_size - 1):#[0 1 2 3 4 5 6 7 8 9]
+                # Q = Q + d*(R[STEP] + G*V[STEP+1]*(1-D) - V[K])
+                delta = reward[step] + GAMMA * critic[step + 1] * (1 - done[step]) - critic[step]
+                returns = returns + discount * delta
                 discount = discount * GAMMA * self.gae_lambda  # D*0.99*0.95
-            advantage[i] = Q
+            advantage[i] = returns#return = return + (G*GAE)^n*DELTA
         advantage = torch.tensor(advantage)
         return advantage
 
+    def compute_gae(next_value, rewards, masks, values, gamma=0.99, tau=0.95):
+        values = values + [next_value]
+        gae = 0
+        returns = []
+        for step in reversed(range(len(rewards))):
+            delta = rewards[step] + gamma * values[step + 1] * masks[step] - values[step]
+            gae = delta + gamma * tau * masks[step] * gae #delta + G * 0.95(1-DONE)*0.
+            returns.insert(0, gae + values[step])
+        return returns
+
     def learn(self):
         for _ in range(self.epoch):
-            state, action, prob, value, reward, done, batch = self.memory.sample()
             ### compute advantage ###
-            advantage = self.compute_advantage(reward, value, done)  # a = a + d*(r + (1-D)*G*V[K+1] - V[K])
+            #state, action, reward, done, prob, critic = self.memory.data()
+            advantage = self.compute_advantage(reward, critic, done)  # a = a + d*(r + (1-D)*G*V[K+1] - V[K])
+            batch = self.memory.sample()
+            print(batch)
+
             ### SGD ###
             values = torch.tensor(value)
             # train_batch()
@@ -151,7 +162,9 @@ class PPO():
                 clip_prob = torch.clamp(prob_ratio, 1 - self.policy_clip, 1 + self.policy_clip) * advantage[batch]
                 actor_loss = -torch.min(weighted_probs, clip_prob).mean()
 
+                # critic_loss = F.smooth_l1_loss(advantage[batch], value.squeeze())
                 critic_loss = ((advantage[batch] + values[batch] - critic_value) ** 2).mean()
+
                 total_loss = actor_loss + 0.5 * critic_loss
 
                 self.actor_optimizer.zero_grad()
@@ -162,14 +175,6 @@ class PPO():
         self.memory.clear()
 
 
-def modify_reward(state):
-    x, x_dot, theta, theta_dot = state  # (位置x，x加速度, 偏移角度theta, 角加速度)
-    r1 = (env.x_threshold - abs(x)) / env.x_threshold - 0.8
-    r2 = (env.theta_threshold_radians - abs(theta)) / env.theta_threshold_radians - 0.5
-    reward = r1 + r2
-    return reward
-
-
 if __name__ == '__main__':
     agent = PPO()
     step = 0
@@ -177,14 +182,12 @@ if __name__ == '__main__':
         state = env.reset()
         epoch_rewards = 0
         while True:
-            # if epoch>200:env.render()
             step = step + 1
-            action, prob, val = agent.choose_action(state)
+            action, prob, critic = agent.choose_action(state)
             next_state, reward, done, _ = env.step(action)
-            # reward = modify_reward(next_state)
-            agent.memory.add(state, action, prob, val, reward, done)
-            # frequency of agent update
-            if step % 64 == 0: agent.learn()
+            agent.memory.add((state, action, reward, done, prob, critic))
+
+            if step % agent.memory.memory_size == 0: agent.learn()
 
             state = next_state
             epoch_rewards = epoch_rewards + reward
