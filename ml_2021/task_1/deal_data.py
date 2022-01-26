2fee0a041bd868d9acde41ccf43da4568bec507b
heyfavourite <619511821@qq.com>
2022-01-26 17:26:00 +0800

    day commit

--- ml_2021/task_1/deal_data.py
+++ ml_2021/task_1/deal_data.py
@@ -12,59 +12,28 @@ class COVID19Dataset(Dataset):
 
     def __init__(self):
         mode = "train"
-        pd_data = pd.read_csv("./covid.train.csv",index_col=["id"])
-        data_y = pd_data.drop("")
-
-        with open("./covid.train.csv", 'r') as fp:
-            data = list(csv.reader(fp))
-            data = np.array(data[1:])[:, 1:].astype(float)
-
-
-        # Training data (train/dev sets)
-        # data: 2700 x 94 (40 states + day 1 (18) + day 2 (18) + day 3 (18))
-        target = data[:, -1]
-        data = data[:, list(range(93))]
-        print(data)
-        print(target)
-
-        # Splitting training data into train & dev sets
-        if mode == 'train':
-            indices = [i for i in range(len(data)) if i % 10 != 0]
-        elif mode == 'dev':
-            indices = [i for i in range(len(data)) if i % 10 == 0]
-
-        # Convert data into PyTorch tensors
-        self.data = torch.FloatTensor(data[indices])
-        self.target = torch.FloatTensor(target[indices])
-        print(self.data)
-        print(self.target)
-        # Normalize features (you may remove this part to see what will happen)
-        self.data[:, 40:] = \
-            (self.data[:, 40:] - self.data[:, 40:].mean(dim=0, keepdim=True)) \
-            / self.data[:, 40:].std(dim=0, keepdim=True)
-
-        self.dim = self.data.shape[1]
-
-        print('Finished reading the {} set of COVID19 Dataset ({} samples found, each dim = {})'
-              .format("train", len(self.data), self.dim))
+        pd_data = pd.read_csv("./covid.train.csv", index_col=["id"])
+        data_y = pd_data["tested_positive.2"]
+        pd_data.drop("tested_positive.2", axis=1, inplace=True)
+        self.data = torch.FloatTensor(pd_data.to_numpy())
+        self.target = torch.FloatTensor(data_y.to_numpy())
+        # 归一化
+        mean = self.data[:, 40:].mean(dim=0, keepdim=True)
+        std = self.data[:, 40:].std(dim=0, keepdim=True)
+        self.data[:, 40:] = (self.data[:, 40:] - mean) / std
 
     def __getitem__(self, index):
-        # Returns one sample at a time
-        if self.mode in ['train', 'dev']:
-            # For training
-            return self.data[index], self.target[index]
-        else:
-            # For testing (no target)
-            return self.data[index]
+        return self.data[index], self.target[index]
 
     def __len__(self):
-        # Returns the size of the dataset
         return len(self.data)
+
 
 def get_dataloader():
     dataset = COVID19Dataset()
-    dataloader = DataLoader(dataset, batch_size=256,shuffle=True)
+    dataloader = DataLoader(dataset, batch_size=256, shuffle=True)
     return dataloader
 
+
 if __name__ == '__main__':
-    tr_set = get_dataloader()
+    dataloader = get_dataloader()
--- ml_2021/task_1/regression.py
+++ ml_2021/task_1/regression.py
@@ -7,3 +7,24 @@ import csv
 import os
 
 
+class NeuralNet(nn.Module):
+    ''' A simple fully-connected deep neural network '''
+
+    def __init__(self, input_dim):
+        super(NeuralNet, self).__init__()
+
+        # Define your neural network here
+        # TODO: How to modify this model to achieve better performance?
+        self.net = nn.Sequential(
+            nn.Linear(input_dim, 64),
+            nn.ReLU(),
+            nn.Linear(64, 1)
+        )
+
+        # Mean squared error loss
+        self.criterion = nn.MSELoss(reduction='mean')
+
+    def forward(self, x):
+        ''' Given input of size (batch_size x input_dim), compute output of the network '''
+        return self.net(x).squeeze(1)
+
