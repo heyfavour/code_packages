b9be964c8506d6618c4e3175dab032af1b872bbb
heyfavourite <619511821@qq.com>
2022-01-27 14:05:31 +0800

    回归commit

--- ml_2021/task_1/regression.py
+++ ml_2021/task_1/regression.py
@@ -1,30 +1,39 @@
 import torch
 import torch.nn as nn
-from torch.utils.data import Dataset, DataLoader
-
-import numpy as np
-import csv
-import os
+from deal_data import get_dataloader
 
 
 class NeuralNet(nn.Module):
-    ''' A simple fully-connected deep neural network '''
-
-    def __init__(self, input_dim):
-        super(NeuralNet, self).__init__()
-
-        # Define your neural network here
-        # TODO: How to modify this model to achieve better performance?
+    def __init__(self):
+        super().__init__()
         self.net = nn.Sequential(
-            nn.Linear(input_dim, 64),
+            nn.Linear(93, 128),
+            nn.Dropout(0.5),
             nn.ReLU(),
-            nn.Linear(64, 1)
+            nn.Linear(128, 1),
         )
 
-        # Mean squared error loss
-        self.criterion = nn.MSELoss(reduction='mean')
+    def forward(self, input):
+        ouput = self.net(input)
+        return ouput
 
-    def forward(self, x):
-        ''' Given input of size (batch_size x input_dim), compute output of the network '''
-        return self.net(x).squeeze(1)
 
+if __name__ == '__main__':
+    dataloader = get_dataloader()
+    model = NeuralNet()
+    criterion = nn.MSELoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.0001)
+    model.train()
+    for epoch in range(30000):
+        _loss = 0
+        for i, (input, labels) in enumerate(dataloader):
+            optimizer.zero_grad()
+            outputs = model(input)  #
+            outputs = outputs.squeeze()
+            loss = criterion(outputs, labels)
+
+            loss.backward()
+            optimizer.step()
+            _loss = _loss + loss.item()
+        print(outputs[:5], labels[:5])
+        print(f"epoch:{epoch} loss:{_loss / (i + 1)}")
