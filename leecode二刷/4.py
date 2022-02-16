bdc1c188dd30a59e3b56f30a44c23e916728d19d
heyfavourite <619511821@qq.com>
2022-02-16 16:31:19 +0800

    lc二刷提交

--- leecode二刷/4.py
+++ leecode二刷/4.py
@@ -1,32 +1,30 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: list[int], nums2: list[int]) -> float:
-        def getKthElement(k):
-            index1, index2 = 0, 0
+        def get_mid_by_k(k):
+            index_1, index_2 = 0, 0
             while True:
-                if index1 == m:return nums2[index2 + k - 1]
-                if index2 == n:return nums1[index1 + k - 1]
-                if k == 1:return min(nums1[index1], nums2[index2])
-                newIndex1 = min(index1 + k // 2 - 1, m - 1)
-                newIndex2 = min(index2 + k // 2 - 1, n - 1)
-                pivot1, pivot2 = nums1[newIndex1], nums2[newIndex2]
-                if pivot1 <= pivot2:
-                    k -= newIndex1 - index1 + 1
-                    index1 = newIndex1 + 1
+                if index_1 == m: return nums2[index_2 + k - 1]
+                if index_2 == n: return nums1[index_1 + k - 1]
+                if k == 1: return min(nums1[index_1], nums2[index_2])
+                tmp_index_1 = min(index_1 + k // 2 - 1, m - 1)
+                tmp_index_2 = min(index_2 + k // 2 - 1, n - 1)
+                if nums1[tmp_index_1] <= nums2[tmp_index_2]:
+                    k = k - (tmp_index_1 - index_1 + 1)
+                    index_1 = tmp_index_1 + 1
                 else:
-                    k -= newIndex2 - index2 + 1
-                    index2 = newIndex2 + 1
+                    k = k - (tmp_index_2 - index_2 + 1)
+                    index_2 = tmp_index_2 + 1
 
         m, n = len(nums1), len(nums2)
-        all = m + n
-
-        if all % 2 == 1:return getKthElement((m + n + 1) // 2) #奇数 只有一个 寻找 (m+ n + 1)//2
-        return (getKthElement(all // 2) + getKthElement(all // 2 + 1)) / 2
-
+        if (m + n) % 2 == 1:
+            return get_mid_by_k((m + n + 1) // 2)
+        else:
+            return (get_mid_by_k((m + n) // 2) + get_mid_by_k((m + n) // 2 + 1)) / 2
 
 
 if __name__ == '__main__':
-    nums1 = [1, 3]
-    nums2 = [2]
+    nums1 = [1, 3, 5, 7, 9]
+    nums2 = [2, 4, 6, 8, 10]
     solution = Solution()
-    median = solution.findMedianSortedArrays(nums1,nums2)
+    median = solution.findMedianSortedArrays(nums1, nums2)
     print(median)
