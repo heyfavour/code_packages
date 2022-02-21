8d9ae308ade2415b47aaeddb466d48b1a7ce3e3c
Mr.Wang <619511821@qq.com>
2022-02-21 20:30:52 +0800

    lc 二刷

--- leecode二刷/8.py
+++ leecode二刷/8.py
@@ -1,27 +1,23 @@
-int_max = 2 ** 32
-int_min = -2 ** 32
-"""
-         ""      +/1   number   other  
-start   start   sign   number    end
-sign    end     end    number    end
-number  end     end    number    end    
-end     end     end    end       end 
-"""
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        s = s.lstrip()
+        ans = 0
+        sign = 1
+        sign_flag = False
+        for i in s:
+            if i  == "+" and not sign_flag:
+                sign = 1
+                sign_flag = True
+            elif i == "-" and not sign_flag:
+                sign = -1
+                sign_flag = True
+            elif i.isdigit():
+                sign_flag = True
+                ans = 10*ans+int(i)
+            else:
+                break
+        ans =  sign*ans
+        if ans>2**31 - 1:return 2**31 -1
+        if ans<-2**31:return -2**31
+        return ans
 
-
-class Automaton():
-    def __init__(self):
-        self.state = "start"
-        self.sign = 1
-        self.ans = 0
-        self.table = {
-            "start": ["start", "sign", "number", "end"],
-            "sign": ["end", "end", "number", "end"],
-            "number": ["end", "end", "number", "end"],
-            "end": ["end", "end", "end", "end"],
-        }
-
-        def get_col(col_str):
-            if col_str.isspace():return 0
-            elif col_str == "+" or col_str == "-":return 1
-            elif col_str
