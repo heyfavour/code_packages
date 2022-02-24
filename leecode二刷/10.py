de543348136ca8d4b9ae380de57737110498d328
Mr.Wang <619511821@qq.com>
2022-02-24 22:57:39 +0800

    lc 二刷

--- leecode二刷/10.py
+++ leecode二刷/10.py
@@ -1,11 +1,18 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        m,n= len(s),len(p)
-        def match(i,j):
-            pass
-        dp = [[False]*(n+1) for _ in range(m+1)]
-        dp[0][0] = True
-        for i in range(m+1):
-            for j in range(1,n+1):
-                if p[j-1] == "*":
-                    dp[i][j] == 
+        m, n = len(s), len(p)
+
+        def is_match(i, j):
+            if j == n: return i == m  # p到了最后，i如果也到最后则匹配否则不匹配
+            first_char_match = (i < m) and (p[j] == '.' or p[j] == s[i])  # i,j对位匹配
+            if j + 1 <= n-1 and p[j + 1] == "*":# p下一位如果是*
+                return is_match(i, j + 2) or is_match(i + 1, j)#0 为 N位 s[i] == p[j+2] s[i+1]=p[j]
+            return first_char_match and is_match(i + 1, j + 1)
+
+        return is_match(0, 0)
+
+if __name__ == '__main__':
+    s = "ab"
+    p = ".*"
+    solution = Solution()
+    print(solution.isMatch(s,p))
