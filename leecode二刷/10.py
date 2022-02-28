a6e8be2b0655b05a4503c3dd0457fd09e7a484cb
Mr.Wang <619511821@qq.com>
2022-02-28 22:56:47 +0800

    lc 二刷

--- leecode二刷/10.py
+++ leecode二刷/10.py
@@ -1,18 +1,24 @@
+from functools import lru_cache
+
+
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         m, n = len(s), len(p)
 
+        @lru_cache(None)
         def is_match(i, j):
+
             if j == n: return i == m  # p到了最后，i如果也到最后则匹配否则不匹配
             first_char_match = (i < m) and (p[j] == '.' or p[j] == s[i])  # i,j对位匹配
-            if j + 1 <= n-1 and p[j + 1] == "*":# p下一位如果是*
-                return is_match(i, j + 2) or is_match(i + 1, j)#0 为 N位 s[i] == p[j+2] s[i+1]=p[j]
+            if j + 1 <= n - 1 and p[j + 1] == "*":  # p下一位如果是*
+                return is_match(i, j + 2) or (first_char_match and is_match(i + 1, j))  # 0 为 N位 s[i] == p[j+2] s[i+1]=p[j]
             return first_char_match and is_match(i + 1, j + 1)
 
         return is_match(0, 0)
 
+
 if __name__ == '__main__':
-    s = "ab"
-    p = ".*"
+    s = "mississippi"
+    p = "mis*is*p*."
     solution = Solution()
-    print(solution.isMatch(s,p))
+    print(solution.isMatch(s, p))
