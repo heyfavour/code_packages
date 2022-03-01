287bce529f4d09ac91b853d140995457d3ae5f50
Mr.Wang <619511821@qq.com>
2022-03-02 00:44:44 +0800

    lc 二刷

--- leecode二刷/22.py
+++ leecode二刷/22.py
@@ -1,5 +1,17 @@
 class Solution:
     def generateParenthesis(self, n: int) -> list[str]:
-        pass
+        ans = []
+        def add_arenthesis(left, right, string):
+            print(left,right,string)
+            if left == 0 and right == 0: ans.append(string)
+            if left > 0: add_arenthesis(left - 1, right, string+"(")
+            if right > 0 and right > left: add_arenthesis(left , right - 1, string+")")
+
+        add_arenthesis(n, n, "")
+        return ans
+
+
 if __name__ == '__main__':
-    pass
+    n = 3
+    solution = Solution()
+    print(solution.generateParenthesis(n))
---
+++ leecode二刷/sort.py
@@ -0,0 +1,10 @@
+import random
+
+
+def quicksort(L):
+    pass
+
+
+if __name__ == '__main__':
+    L = [random.randint(1, 100) for i in range(50000)]
+    quicksort(l)
