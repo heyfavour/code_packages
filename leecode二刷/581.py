206ddf872966ee1781886cef3a0be0c48627a7fe
Mr.Wang <619511821@qq.com>
2022-10-25 00:11:12 +0800

    lc二刷提交

--- leecode二刷/581.py
+++ leecode二刷/581.py
@@ -1,31 +1,24 @@
 class Solution:
     def findUnsortedSubarray(self, nums: list[int]) -> int:
         n = len(nums)
-        max = nums[-1]
-        first = False
-        l = n-1
-        for i in range(n-1,-1,-1):
-            if i>=1 and nums[i]<nums[i-1]:
-                max = nums[i]
-                first = True
-            if nums[i]<max and first:
-                l = i
-                first = False
-        min = nums[0]
-        first = False
+        if n <= 1: return 0
+        # L 最左侧 找到一个树 比右边所有的数都小
+        rightmin = float("inf")
+        l = n - 1
+        for i in range(n - 1, -1, -1):
+            if nums[i] > rightmin:
+                l = i - 1
+            rightmin = min(nums[i], rightmin)
+        # R 最右侧 找到一个比左边都大的树
+        leftmax = float("-inf")
         r = 0
         for i in range(n):
-            if i<=n-2 and nums[i]>nums[i+1]:
-                min = nums[i]
-                first = True
-            if nums[i]>min and first:
-                r = i
-                first = False
-        print(l,r)
-        if l>=r:return 0
-        return r-l-1
+            if nums[i] < leftmax: r = i + 1
+            leftmax = max(leftmax, nums[i])
+        return r - l - 1 if r > l else 0
+
 
 if __name__ == '__main__':
     solution = Solution()
-    #print(solution.findUnsortedSubarray(nums = [2,6,4,8,10,9,15]))
-    print(solution.findUnsortedSubarray(nums = [2,1]))
+    print(solution.findUnsortedSubarray(nums=[2, 6, 4, 8, 10, 9, 15]))
+    # print(solution.findUnsortedSubarray(nums=[2, 1]))
