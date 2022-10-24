43cf135b2c8f16e06c61ea03c6f3b52ff95b9e95
Mr.Wang <619511821@qq.com>
2022-10-25 01:14:50 +0800

    lc二刷提交

---
+++ leecode二刷/621.py
@@ -0,0 +1,10 @@
+class Solution:
+    def leastInterval(self, tasks: list[str], n: int) -> int:
+        pass
+
+
+if __name__ == '__main__':
+    tasks = ["A", "A", "A", "B", "B", "B"]
+    n = 2
+    solution = Solution()
+    print(solution.leastInterval(tasks, n))
