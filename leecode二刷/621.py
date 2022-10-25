88702266c3c2568148f52253396281f4a015461b
heyfavourite <619511821@qq.com>
2022-10-25 17:12:28 +0800

    lc二刷提交

--- leecode二刷/621.py
+++ leecode二刷/621.py
@@ -1,6 +1,15 @@
+import collections
+
+
 class Solution:
     def leastInterval(self, tasks: list[str], n: int) -> int:
-        pass
+        task_count = collections.Counter(tasks)
+        tasks_count = sorted(task_count.values(), reverse=True)
+        max_count = tasks_count[0]
+        col = 0
+        for v in tasks_count:
+            if v == max_count: col = col + 1
+        return max(len(tasks_count), (max_count - 1) * (n + 1) + col)
 
 
 if __name__ == '__main__':
